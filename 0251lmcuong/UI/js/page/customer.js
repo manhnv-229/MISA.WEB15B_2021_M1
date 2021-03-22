6e65edc1a57d78fd3cdd041ae65799d0b64bd5d1
lmcuong <49238082+cuongoo1752@users.noreply.github.com>
2021-03-22 13:22:38 +0700

    bat tap buoi 5

--- 0251lmcuong/UI/js/page/customer.js
+++ 0251lmcuong/UI/js/page/customer.js
@@ -1,3 +1,5 @@
+//Hiện và ẩn phần thêm khách hàng
+
 var hiden = document.querySelectorAll(".hiden");
 hiden[0].style.display = "none";
 
@@ -19,3 +21,58 @@ btnCancel[0].onclick = () => {
     hiden[0].style.display = "none";
     dialog[0].style.display = "none";
 }
+
+$(document).ready(() => {
+    setEvent();
+    loadData(1);
+});
+
+function setEvent(){
+
+}
+
+function loadData(){
+    var data = getData();
+    // console.table(data);
+    displayTableData(data);
+}
+
+function getData(){
+    var data;
+    $.ajax({
+        method: 'GET',
+        url: 'http://api.manhnv.net/api/customers',
+        async: false,
+        contentType: "application/json",
+        success: function(response){
+            data = response;
+        },
+        error: function(response){
+            alert("Lấy dữ liệu thất bại")
+        }
+    })
+    return data;
+}
+
+function displayTableData(data){
+    $.each(data, function(index, customer){
+        var rowHTML = $(`
+        <tr>
+            <td>${customer.CustomerCode}</td>
+            <td>${customer.FullName}</td>
+            <td>${customer.Gender}</td>
+            <td>${customer.DateOfBirth}</td>
+            <td>${customer.CustomerGroupName}</td>
+            <td>${customer.PhoneNumber}</td>
+            <td>${customer.Email}</td>
+            <td>${customer.Email}</td>
+            <td>${customer.DebitAmount}</td>
+            <td>${customer.MemberCardCode}</td>
+        </tr>
+        `);
+        $(".content-grid .content-grid-table").append(rowHTML);
+    })
+    //$(".content-grid .content-grid-table")
+}
+
+
